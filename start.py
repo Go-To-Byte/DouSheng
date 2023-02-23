85868644160b127fe5809b28b04babe43fb54f34
novice2194 <2194150786@qq.com>
2023-02-23 23:24:48 +0800

    fix: some bug

--- start.py
+++ start.py
@@ -53,7 +53,7 @@ def run_main(path):
     print(f"Now path: {os.getcwd()}\n"
           f"RUN {path.name}, PID: {os.getpid()}")
     os.system(f"go run {path}/main.go >> ~/log/{path.name}.log")
-    os.system(f"echo {os.getpid()} >> {pathlib.Path().home()}/log/pid.txt")
+    os.system(f"echo {os.getpid()} >> ~/log/pid.txt")
 
 
 def run_all_main():
