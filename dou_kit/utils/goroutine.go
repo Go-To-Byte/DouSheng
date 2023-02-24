5289645710d40113cca7b36e634be316d5c09912
novice2194 <2194150786@qq.com>
2023-02-24 22:40:29 +0800

    feat: add utils of goroutine

--- dou_kit/utils/goroutine.go
+++ dou_kit/utils/goroutine.go
@@ -3,3 +3,78 @@
 // Software: GoLand
 
 package utils
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+// GoRunGrpc is used run a goroutine of grpc
+// You need give context, grpc function and request
+// the grpc's return is filled with response and error
+// Example:
+//
+//	wait := sync.WaitGroup{}
+//	wait.Add(1)
+//	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+//	defer cancel()
+//	run := GoRunGrpc{
+//		Ctx:      &ctx,
+//		Wait:     &wait,
+//		Request:  user.NewUserInfoRequest(),
+//		Rpc:      h.service.UserInfo,
+//	}
+//	go run.Run()
+type GoRunGrpc struct {
+	Err      error
+	Ctx      *context.Context
+	Wait     *sync.WaitGroup
+	Request  interface{}
+	Response interface{}
+	Rpc      func(interface{}, interface{}) (interface{}, error) // grpc function
+}
+
+func NewGoRunGrpc() GoRunGrpc {
+	return GoRunGrpc{}
+}
+
+func (g GoRunGrpc) Run() {
+	defer g.Wait.Done()
+	for {
+		select {
+		case <-(*g.Ctx).Done():
+			return
+		default:
+			g.Response, g.Err = g.Rpc(*g.Ctx, g.Request)
+		}
+	}
+}
+
+func GORUN(ctx *context.Context, wait *sync.WaitGroup,
+	rpc func(interface{}, interface{}) (interface{}, error),
+	req interface{}) (resp interface{}, err error) {
+	defer wait.Done()
+	for {
+		select {
+		case <-(*ctx).Done():
+			return
+		default:
+			resp, err = rpc(*ctx, req)
+		}
+	}
+}
+
+func example() {
+	wait := sync.WaitGroup{}
+	wait.Add(1)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	run := GoRunGrpc{
+		Ctx:     &ctx,
+		Wait:    &wait,
+		Request: nil,
+		Rpc:     nil,
+	}
+	go run.Run()
+}
