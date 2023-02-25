20de96ad0b1f9ae6b24ca14d2b6c72b2c2a3ddc7
novice2194 <2194150786@qq.com>
2023-02-25 18:25:10 +0800

    perf: GORUN

--- dou_kit/utils/goroutine.go
+++ dou_kit/utils/goroutine.go
@@ -30,9 +30,9 @@ type GoRunGrpc struct {
 	Err      error
 	Ctx      *context.Context
 	Wait     *sync.WaitGroup
-	Request  interface{}
-	Response interface{}
-	Rpc      func(interface{}, interface{}) (interface{}, error) // grpc function
+	Request  any
+	Response any
+	Rpc      func(ctx context.Context, request any) (response any, err error) // grpc function
 }
 
 func NewGoRunGrpc() GoRunGrpc {
@@ -51,16 +51,36 @@ func (g GoRunGrpc) Run() {
 	}
 }
 
-func GORUN(ctx *context.Context, wait *sync.WaitGroup,
-	rpc func(interface{}, interface{}) (interface{}, error),
-	req interface{}) (resp interface{}, err error) {
+// GORUN You need give context, grpc function, request, and response.
+//
+// rpc is a function:
+//
+//	func(ctx context.Context, request interface{}) (response interface{}, err error)
+//
+// response is the return value of rpc function, it must be a pointer.
+//
+// Example:
+//
+//	wait := sync.WaitGroup{}
+//	wait.Add(1)
+//	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+//	go GORUN(ctx, wait, user.NewUserInfoRequest, h.service.UserInfo, userInfoResp{})
+func GORUN(
+	ctx *context.Context, // context.WithTimeout()
+	wait *sync.WaitGroup, // sync.WaitGroup{}
+	rpc func(ctx context.Context, request any) (response any, err error),
+	req any,
+	resp *any,
+) (err error) {
 	defer wait.Done()
 	for {
 		select {
 		case <-(*ctx).Done():
 			return
 		default:
-			resp, err = rpc(*ctx, req)
+			r, e := rpc(*ctx, req)
+			resp = &r
+			err = e
 		}
 	}
 }
