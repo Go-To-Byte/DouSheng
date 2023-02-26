c749cdb7340ceb6ca221541629c6077abc6e542e
novice2194 <2194150786@qq.com>
2023-02-26 22:39:13 +0800

    deleted: grpc goroutine util

--- dou_kit/utils/utils_test.go
+++ dou_kit/utils/utils_test.go
@@ -2,10 +2,12 @@
 package utils_test
 
 import (
-	"sync"
-	"testing"
-
+	"context"
+	"fmt"
 	"github.com/Go-To-Byte/DouSheng/dou_kit/utils"
+	"reflect"
+	"testing"
+	"time"
 )
 
 func TestSet(t *testing.T) {
@@ -20,11 +22,29 @@ func TestSet(t *testing.T) {
 	t.Log(items)
 }
 
+type rpcImpl interface {
+	func1(ctx context.Context, req int64) (resp int64, err error)
+}
+type rpc struct {
+	request  int64
+	response int64
+}
+
+func (r rpc) func1(ctx context.Context, req int64) (resp int64, err error) {
+	fmt.Println("ok: ", req)
+	r.request = req
+	r.response = req * 10
+	return req * 10, nil
+}
+
 func TestGoRunGrpc(t *testing.T) {
-	var waitGroup []*sync.WaitGroup
-	run, _ := utils.NewGoRunGrpc(&waitGroup, nil, nil)
-	go run.Run()
-	for _, wait := range waitGroup {
-		(*wait).Wait()
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r := rpc{}
+	v := reflect.ValueOf(r.func1)
+	var args []reflect.Value
+	args = append(args, reflect.ValueOf(ctx))
+	args = append(args, reflect.ValueOf(int64(1)))
+	c := v.Call(args)
+	fmt.Println("ok: ", c[0].Type().String(), c[0].Interface().(int64))
 }
